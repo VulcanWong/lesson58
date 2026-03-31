63ba5a94ec5a89d97a6e60d6f3674742bdfa5009
VulcanWong <wongvulcan@gmail.com>
2026-03-31 20:07:37 +0800

    lesson58

---
+++ activity2.py
@@ -0,0 +1,7 @@
+def print_factors(number):
+    print("The factor of",number,"are:")
+    for i in range(1, number +1):
+        if number %  i == 0:
+            print(i)
+number = int(input("Enter your number to find it's factor: "))
+print_factors(number)
